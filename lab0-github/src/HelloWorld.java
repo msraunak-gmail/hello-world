870d35181b2d641564f57c1c61070a1c2b0cbad6
raunak <raunak@loyola.edu>
2017-09-06 11:09:48 -0400

    First commit of Hello World

---
+++ lab0-github/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * The time-honored HelloWorld Java Program
+ * @author raunak
+ * @version 1.0 9/6/2017
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
